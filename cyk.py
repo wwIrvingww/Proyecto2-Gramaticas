ad14ddc35b841dfce75eca7af7668fafd6b382e5
MarchMol <marchmol98@gmail.com>
2024-10-23 19:49:37 -0600

    feat: CYK algorithm

---
+++ cyk.py
@@ -0,0 +1,47 @@
+def printMatrix(matrix, cadena):
+    for i in range(len(matrix)):
+        tem = cadena[i]+" "
+        for j in range(len(matrix[i])):
+            tem=tem+str(matrix[i][j])+", "
+        print(tem)
+
+def cyk_main(CNF, w):
+    n = len(w)
+    matrix = []
+    
+    # Inicialización de la matriz de cyk. 
+    # tamaño nxn con todas las entradas siendo listas vacias
+    for _ in range(n):
+        tem = []
+        for _ in range(n):
+            tem.append([])
+        matrix.append(tem)
+
+    # Se itera por la identidad de la matriz
+    for i in range(n):
+        for var in CNF["V"]:
+            if(w[i] in CNF["P"][var]):
+                matrix[i][i].append(var)
+            
+    # CYK other iteration     
+    for l in range(1,n):
+        for i in range(n-l):
+            j = i+l
+            for k in range(i,j):
+                for var in CNF["V"]:
+                    for rsltVar in CNF["P"][var]:
+                        if(len(rsltVar)>1):
+                            print(rsltVar[0],matrix[i][k])
+                            print(rsltVar[1],matrix[k+1][j])
+                            print("")
+                            if(rsltVar[0] in matrix[i][k]) and (rsltVar[1] in matrix[k+1][j]):
+                                if not (var in matrix[i][j]):
+                                    matrix[i][j].append(var)
+            
+    printMatrix(matrix, w)
+    if(CNF["S"] in matrix[0][n-1]):
+        print("la cadena es acepatada")
+        return (True, matrix)
+    else:
+        print("La cadena NO es acepatada")
+        return (False, [])
